b2ef4f84ba65a85781c765fcbe67108196d8b1d9
igoravila93 <igoravila93@gmail.com>
2021-08-26 13:49:19 -0300

    Primeira seção da aula

--- js/main.js
+++ js/main.js
@@ -3,5 +3,51 @@ var nome = "Igor Avila";
 alert("Bem vindo " + nome);
 */
 
-var nome = "Igor Avila";
-alert("Bem vindo " + nome);
+//tipos de variaveis são dinamicas 10 é int, "10" é string
+//operações são simples
+var nome = "Igor Avila"
+
+//lista 
+var lista_fruta = ["maçã", "laranja", "melancia"]
+    //lista_fruta.pop("laranja") isso não faz sentido o pop sempre remove ultimo elemento
+console.log(lista_fruta)
+    //lista.push("pera")
+    //lista.pop remove ultimo elemento
+    //lista.lenght
+    //lista.reverse
+    //lista.toString()
+    //lista.join("|") pra transformar em string com o termo desejado separando os elementos
+
+//dicionario e lista
+var frutas = [{ nome: "maçã", cor: "vermelho" }, { nome: "laranja", cor: "laranja" }]
+
+//data - tem getDay, getMonth, etc
+var d = new Date()
+
+//prompt de pergunta
+//var idade = prompt("Qual sua idade") //abre popup perguntando idade
+
+//condicional
+// if (idade >= 18) {
+//     alert("maior de idade")
+// } else {
+//     alert("menor de idade")
+// }
+
+//repetição
+// var count = 0
+// while (count < 5) {
+//     console.log(count)
+//     count++
+// }
+// for (var count = 0; count < 5; count++) {
+//     console.log(count)
+// }
+
+
+//alerta na página 
+//alert("Bem vindo " + nome)
+
+//Print no console
+//console.log(nome + " é demais!")
+//console.log(nome.replace("Avila", "Souza") + " é como ta no Linkedin")
